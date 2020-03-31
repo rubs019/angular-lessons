36e4f518256cd78173c1c2a05890d22e7a3d34a9
Ruben DESERT <ruben.desert@eemi.com>
2020-03-31 11:59:12 +0200

    WIP - pokemon selectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { PokemonSelectorComponent } from './pokemon-selector/pokemon-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PokemonSelectorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pokemon-selector/pokemon-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-selector',
+  templateUrl: './pokemon-selector.component.html',
+  styleUrls: ['./pokemon-selector.component.css']
+})
+export class PokemonSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
