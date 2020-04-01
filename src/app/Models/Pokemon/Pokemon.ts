3577f5946943d4c3853e43a64dd731a0c448a729
Yanis <yanisakli@yahoo.fr>
2020-04-01 09:58:13 +0200

    :fire: Add BeansToPokemon

--- src/app/Models/Pokemon/Pokemon.ts
+++ src/app/Models/Pokemon/Pokemon.ts
@@ -1,7 +1,9 @@
 import { Attack } from '../Attack/Attack.definition';
 import { IPokemon } from './IPokemon';
+import { PokemonBeans } from './pokemonBeans';
 
 export class Pokemon {
+
   id: number;
   name: string;
   level: number;
@@ -12,8 +14,6 @@ export class Pokemon {
   health: number;
   basePower: number;
   attacks: Attack[];
-  specOffStat: number;
-  specDefStat: number;
 
   constructor(pokemon: IPokemon) {
     this.id = pokemon?.id;
@@ -26,9 +26,32 @@ export class Pokemon {
     this.basePower = pokemon?.basePower;
     this.health = this.maxHealth;
     this.attacks = pokemon?.attacks;
-    this.specOffStat = pokemon?.specOffStat;
-    this.specDefStat = pokemon?.specDefStat;
   }
+
+  public static BeansToPokemon(pokemonBeans: PokemonBeans): Pokemon {
+    const attacks = pokemonBeans.moves
+        .slice(0, 10)
+        .map(m => new Attack({
+            name: m.move.name,
+            basePower: pokemonBeans.stats.find( s => s.stats.name === 'attack').base_stat
+        }));
+    const speed = pokemonBeans.stats.find( s => s.stats.name === 'speed').base_stat;
+    const hp = pokemonBeans.stats.find( s => s.stats.name === 'hp').base_stat;
+    const offStat = pokemonBeans.stats.find( s => s.stats.name === 'attack').base_stat;
+    const defStat = pokemonBeans.stats.find( s => s.stats.name === 'defense').base_stat;
+
+    return new Pokemon({
+        id: pokemonBeans.id,
+        name: pokemonBeans.name,
+        level: 10,
+        speed,
+        offStat,
+        defStat,
+        maxHealth: hp,
+        attacks,
+    });
+
+}
 
   public loseHealth(damage: number): void {
     this.health -= damage;
@@ -36,6 +59,4 @@ export class Pokemon {
       this.health = 0;
     }
   }
-
-
 }
