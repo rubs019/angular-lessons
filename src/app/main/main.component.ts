8dd90359c3f49c64748e104813b78b4fcda988bb
Yanis <yanisakli@yahoo.fr>
2020-04-01 15:27:20 +0200

    :wrench: getPokemons

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../Services/api.service';
+import { Pokemon } from '../Models/Pokemon/Pokemon';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  providers: [ ApiService ]
 })
 export class MainComponent implements OnInit {
 
   pokemons = ['Pikachu', 'Bulbizarre', 'Pikachu', 'Bulbizarre', 'Pikachu', 'Bulbizarre', 'Pikachu', 'Bulbizarre'];
-  constructor() { }
+  constructor(private apiService: ApiService) {
+
+    apiService.getPokemons().subscribe((pokemons: Pokemon[]) => {
+      console.log('Pokemon', pokemons[1].name);
+    });
+  }
 
   ngOnInit(): void {
   }
